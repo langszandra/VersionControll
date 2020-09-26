40a850e4bb23764a84ada91b43d2e290ef0909ae
langalexandra998@gmail.com <langalexandra998@gmail.com>
2020-09-26 16:02:04 +0200

    savfile

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -9,8 +9,7 @@ namespace UserMaintenance.Entities
     class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string FirstName;
-        public string LastName;
+     
         public string FullName { get; set; }
         
     }
