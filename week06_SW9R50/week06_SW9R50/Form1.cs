5ccb9c8699315faf27c19f3d5649c9bae5f2301a
langalexandra998@gmail.com <langalexandra998@gmail.com>
2020-10-17 17:44:39 +0200

    XML feldolgozasa

--- week06_SW9R50/week06_SW9R50/Form1.cs
+++ week06_SW9R50/week06_SW9R50/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week06_SW9R50.Entities;
 using week06_SW9R50.MnbServiceReference;
 
@@ -20,7 +21,14 @@ namespace week06_SW9R50
         {
             InitializeComponent();
             dataGridView1.DataSource = Rates;
+            getExchangeRates();         
+
+
+        }
+        private void getExchangeRates()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
+
 
             var request = new GetExchangeRatesRequestBody()
             {
@@ -32,6 +40,31 @@ namespace week06_SW9R50
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+           
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                
+                var rate = new RateData();
+                Rates.Add(rate);
+
+              
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+
+
         }
     
     }
