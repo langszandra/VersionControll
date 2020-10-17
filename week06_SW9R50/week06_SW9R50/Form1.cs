19fcff112fc02154ad46574d48f9073a6f78cc4d
langalexandra998@gmail.com <langalexandra998@gmail.com>
2020-10-17 17:49:48 +0200

    Adatpok megjelenítése diagrammon

--- week06_SW9R50/week06_SW9R50/Form1.cs
+++ week06_SW9R50/week06_SW9R50/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week06_SW9R50.Entities;
 using week06_SW9R50.MnbServiceReference;
@@ -62,6 +63,22 @@ namespace week06_SW9R50
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
+
+                chartRateData.DataSource = Rates;
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
             }
 
 
