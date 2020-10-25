8659e70f2a94e109109d27e91f0051dfb0a6f01b
langalexandra998@gmail.com <langalexandra998@gmail.com>
2020-10-25 12:02:22 +0100

    osztályok létrehozása

---
+++ week07_SW9R50/week07_SW9R50/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07_SW9R50.Entities
+{
+    class BirthProbability
+    {
+        public int age { get; set; }
+        public int children { get; set; }
+        public double probability { get; set; }
+    }
+}
---
+++ week07_SW9R50/week07_SW9R50/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07_SW9R50.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int age { get; set; }
+
+        public double deathprobability { get; set; }
+    }
+}
